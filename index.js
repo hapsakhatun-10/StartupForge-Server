71324d6bf918b2d86dbb98e3b73b42d8cb47ea20
hapsakhatun-10 <hk.hapsakhatun@gmail.com>
2026-06-21 22:16:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://startupforge:sBoEQqhbCQMwodzP@cluster0.ku35jgw.mongodb.net/?appName=Cluster0";
+
+
+
+const app = express()
+
+const PORT = 5000
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+
+    res.send("Server is running fine!")
+})
+
+
+
+
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server is Running on port ${PORT}`)
+})
